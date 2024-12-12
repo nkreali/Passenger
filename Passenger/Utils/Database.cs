6b94aca5a97f2a450c148bc5b56745295dc0197b
Нурали Курманов <nurali.kurmanov@mail.ru>
2024-12-12 08:13:51 +0500

    backup

--- Passenger/Utils/Database.cs
+++ Passenger/Utils/Database.cs
@@ -6,19 +6,58 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
 using PassengerLib;
+using System.Security;
 
 namespace Passenger.Utils
 {
-    public class Database
+    public static class Database
     {
+        private static readonly string DefaultPath = @".\data.db";
         public static void InitializeTables()
         {
-            
+            if (!File.Exists(DefaultPath))
+            {
+                using var connection = new SQLiteConnection($"Data source={DefaultPath}");
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand(connection);
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Users(" +
+                    "Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "Login TEXT NOT NULL, " +
+                    "Password TEXT NOT NULL)";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Accounts(" +
+                    "Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "OwnerId Integer NOT NULL, " +
+                    "Service TEXT NOT NULL, Login TEXT NOT NULL, " +
+                    "Password TEXT NOT NULL, " +
+                    "FOREIGN KEY(OwnerId) REFERENCES Users (Id))";
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
         }
 
-        public Database()
+        public static void RegisterUser(string login, SecureString password)
+        {
+            using var connection = new SQLiteConnection($"Data source ={DefaultPath}");
+            connection.Open();
+
+            connection.Close();
+        }
+
+        public static List<Accounts> AuthorizeUser()
+        {
+            return new List<Accounts>;
+        }
+        
+        public static void DeleteUser()
         {
 
         }
+
+        public static void AddAccount() { }
+
+        public static void DeleteAccount() { }
+
     }
 }
---
+++ Passenger/Utils/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passenger.Utils
+{
+    public class User
+    {
+        public int Id { get; set; }
+        private string login, password;
+
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+
+    }
+}
